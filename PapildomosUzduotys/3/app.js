14670ab861ccf470c0eeedf2d06f98a4fba3210e
Marius258 <marius.kavaliauskas147@gmail.com>
2022-07-03 18:16:21 +0300

    basic dice simulator

--- PapildomosUzduotys/3/app.js
+++ PapildomosUzduotys/3/app.js
@@ -1,54 +1,45 @@
-const numOfRolls = 100000
-const numOfDice = 10
-const expectedSum = 60
+const numOfRolls = 100000000
 
-//calculates the total sum of all dice version 2
-const getDiceSum = () => {
-     const max = numOfDice * 6
-     const min = numOfDice
-     const diceSum = Math.floor(Math.random() * (max - min + 1) + min)
-     return diceSum
+const button = document.querySelector('#rollDice')
+const numOfDiceInput = document.querySelector('#numOfDiceInput')
+const expectedSumInput = document.querySelector('#expectedSumInput')
+const display = document.querySelector('#display')
+
+button.addEventListener('click', () => {
+     probabilityDisplay(rollDice())
+})
+
+const rollDice = () => {
+     const numOfDice = parseInt(numOfDiceInput.value)
+     const expectedSum = parseInt(expectedSumInput.value)
+     if (expectedSumInput.value !== NaN && numOfDice !== NaN) {
+          return getMatchingRolls(numOfDice, expectedSum)
+     }
 }
 
-const getMatchingRolls = () => {
+// generates random sum of dice
+const getDiceSum = (numOfDice) => {
+     const max = numOfDice * 6
+     const min = numOfDice
+     return (diceSum = Math.floor(Math.random() * (max - min + 1) + min))
+}
+
+//simulates the dice throw and comapres to expected sum
+const getMatchingRolls = (numOfDice, expectedSum) => {
      let matchingRolls = 0
      for (i = 0; i < numOfRolls; i++) {
-          let diceSum = getDiceSum()
-          if (diceSum === expectedSum) {
+          const diceSum = getDiceSum(numOfDice)
+          if (expectedSum === diceSum) {
                matchingRolls += 1
           }
      }
-     return matchingRolls
-}
 
-const calculateProbability = () => {
-     const matchingRolls = getMatchingRolls()
      return matchingRolls / numOfRolls
 }
 
-console.log(calculateProbability() * 100)
+// UI
 
-// used to debug random dice sum generator
-// let hits = 0
-
-// for (i = 0; i < 1000; i++) {
-//      let max = 10
-//      let dice = getDiceSum2()
-//      if (dice <= max) {
-//           hits += 1
-//      }
-// }
-// console.log(`hits ${hits}`)
-// *****************************************************************
-// calculates the total sum of all dice version 1
-// generates a single dice
-// const generateDice = () => {
-//      return (dice = Math.floor(Math.random() * 6) + 1)
-// }
-// const getDiceSum = () => {
-//      let diceSum = 0
-//      for (i = 0; i < numOfDice; i++) {
-//           diceSum += generateDice()
-//      }
-//      return diceSum
-// }
+const probabilityDisplay = (value) => {
+     const message = value * 100
+     display.textContent = message + '%'
+}
